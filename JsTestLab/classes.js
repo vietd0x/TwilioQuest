cadd0eca32fdc5fe77ed9156777430e8fd4cb9ff
VietDo <viet.doxv@gmail.com>
2021-02-09 01:02:15 +0700

    Class Art

--- JsTestLab/classes.js
+++ JsTestLab/classes.js
@@ -1,3 +1,26 @@
 class Materializer{
+    constructor(targetName){
+        this.target = targetName;
+        this.activated = false;
+    }
     
+    activate(){
+        this.activated = true;
+    }
+    materialize(){
+        if(this.activated){
+            return this.target;
+        }
+        else
+            return undefined;
+    }
 }
+
+// used by you to test your solution.
+const m = new Materializer('Kevin');
+console.log(m.activated); // would print "false"
+
+m.activate();
+console.log(m.activated); // would print "true"
+
+console.log(m.materialize()); // would print "Kevin"
